eeebb6fa1c31c8242c38a52b841686782fd497b6
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-09-13 14:15:07 -0400

    yjf

--- raspberry-pi/code.py
+++ raspberry-pi/code.py
@@ -2,21 +2,23 @@
 import board
 import digitalio
 import time
-led = digitalio.DigitalInOut(GP13.LED)
-led.direction = digitalio.Direction.OUTPUT
-led = digitalio.DigitalInOut(GP18.LED)
-led.direction = digitalio.Direction.OUTPUT
+ledred = digitalio.DigitalInOut(board.GP13)
+ledred.direction = digitalio.Direction.OUTPUT
+ledgreen = digitalio.DigitalInOut(board.GP18)
+ledgreen.direction = digitalio.Direction.OUTPUT
 
 
-led.value = True
-led.value = False
-
-while True:
-  for x in range (10, 0, -1):
-   print(x)
-   led.value = True
-   time.sleep(1)
-   led.value = False
-   time.sleep(1)
-  print("Lift off")
+for x in range (10, 0, -1):
+   if x != 0:
+    ledgreen.value = True
+    time.sleep(1)
+    ledgreen.value = False
+    time.sleep(1)
+ 
+   else: 
+    print(x)
+    ledred.value = False 
+    time.sleep(1)
+    ledred.value = True
+    time.sleep(1)
 
