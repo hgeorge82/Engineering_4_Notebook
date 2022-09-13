b25d661a5bbe5036fd0c3d2fc34cc83e3199bf4a
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-09-13 14:17:55 -0400

    trgf

--- raspberry-pi/code.py
+++ raspberry-pi/code.py
@@ -2,23 +2,23 @@
 import board
 import digitalio
 import time
-ledred = digitalio.DigitalInOut(board.GP13)
-ledred.direction = digitalio.Direction.OUTPUT
 ledgreen = digitalio.DigitalInOut(board.GP18)
 ledgreen.direction = digitalio.Direction.OUTPUT
+ledred = digitalio.DigitalInOut(board.GP13)
+ledred.direction = digitalio.Direction.OUTPUT
 
 
 for x in range (10, 0, -1):
    if x != 0:
-    ledgreen.value = True
+    ledred.value = True
     time.sleep(1)
-    ledgreen.value = False
+    ledred.value = False
     time.sleep(1)
  
    else: 
     print(x)
-    ledred.value = False 
+    ledgreen.value = False 
     time.sleep(1)
-    ledred.value = True
+    ledgreen.value = True
     time.sleep(1)
 
