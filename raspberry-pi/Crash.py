9c26319c60b79ed9179ebb9fc37eee420dcba82e
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-09-23 13:50:10 -0400

    avoidance

--- raspberry-pi/Crash.py
+++ raspberry-pi/Crash.py
@@ -1,13 +1,12 @@
 #type: ignore
 import adafruit_mpu6050
 import busio
+import board
 
 sda_pin = board.GP14
 scl_pin = board.GP15
-i2c = busio.I2C(GP.15, GP.14)
-
+i2c = busio.I2C(scl_pin, sda_pin)
 
 mpu = adafruit_mpu6050.MPU6050(i2c)
-mpu.acceleration
-mpu.acceleration[0]
-mpu.gyro
+print(mpu.acceleration)
+
