1b084815632f016b6ec00016d21e70acacedf4fd
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-09-30 14:08:57 -0400

    powerboost

--- raspberry-pi/Crash2.py
+++ raspberry-pi/Crash2.py
@@ -10,10 +10,29 @@ ledred.direction = digitalio.Direction.OUTPUT
 sda_pin = board.GP14
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
+mpu = adafruit_mpu6050.MPU6050(i2c)
 
 while True:
-   if mpu.acceleration > 9
-   ledred.value 
-   time.sleep()
-   ledred.value
+   if mpu.acceleration > (9):
+      ledred.value = True
+      print(mpu.acceleration[0])
+      ledred.value = False
+      
+   if mpu.acceleration < (-9):
+      ledred.value = True
+      print(mpu.acceleration[0])
+      ledred.value = False
+      
+   if mpu.acceleration > (-9):
+      ledred.value = True
+      print(mpu.acceleration[0])
+      ledred.value = False
+      
+   if mpu.acceleration < (9):
+      ledred.value = True
+      print(mpu.acceleration[0])
+      ledred.value = False
+    
+    
+ 
     
