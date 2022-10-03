811cb0ea71ff1a87fbd05b138a43318e212e291f
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-03 13:42:20 -0400

    Powerboost

--- raspberry-pi/Crash2.py
+++ raspberry-pi/Crash2.py
@@ -13,22 +13,22 @@ i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
 while True:
-   if mpu.acceleration > (9):
+   if mpu.acceleration[0] > (9):
       ledred.value = True
       print(mpu.acceleration[0])
       ledred.value = False
       
-   if mpu.acceleration < (-9):
+   if mpu.acceleration[0] < (-9):
       ledred.value = True
       print(mpu.acceleration[0])
       ledred.value = False
       
-   if mpu.acceleration > (-9):
+   if mpu.acceleration[0] > (9):
       ledred.value = True
       print(mpu.acceleration[0])
       ledred.value = False
       
-   if mpu.acceleration < (9):
+   if mpu.acceleration[0] < (-9):
       ledred.value = True
       print(mpu.acceleration[0])
       ledred.value = False
