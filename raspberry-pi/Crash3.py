532387415a238d0acd2f57259fc47a80b4d86085
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-04 14:22:47 -0400

    OLED

--- raspberry-pi/Crash3.py
+++ raspberry-pi/Crash3.py
@@ -8,47 +8,49 @@ import board
 import time
 import busio
 
-
+displayio.release_displays()
 sda_pin = board.GP14
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
 
-displayio.release_displays()
+
 display_bus = displayio.I2CDisplay(i2c, device_address= 0x3d , reset=board.GP18)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 mpu = adafruit_mpu6050.MPU6050(i2c, address= 0x68 )
 
-# create the display group
-splash = displayio.Group()
 
-# add title block to display group
-title = "ANGULAR VELOCITY"
-# the order of this command is (font, text, text color, and location)
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
-splash.append(text_area)    
+while True:
+  # create the display group
+  splash = displayio.Group()
 
-
-title = "ANGULAR VELOCITY"
-# the order of this command is (font, text, text color, and location)
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=10)
-splash.append(text_area)
+  # add title block to display group
+  title = "ANGULAR VELOCITY"
+  # the order of this command is (font, text, text color, and location)
+  text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=20)
+  splash.append(text_area)    
 
 
-title = "ANGULAR VELOCITY"
-# the order of this command is (font, text, text color, and location)
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=15)
-splash.append(text_area)
+  title = f"x{mpu.gyro[0]} rad/s"
+  # the order of this command is (font, text, text color, and location)
+  text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=40)
+  splash.append(text_area)
 
 
-title = "ANGULAR VELOCITY"
-# the order of this command is (font, text, text color, and location)
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=20)
-splash.append(text_area)
+  title = f"y{mpu.gyro[1]} rad/s"
+  # the order of this command is (font, text, text color, and location)
+  text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=60)
+  splash.append(text_area)
 
-# you will write more code here that prints the x, y, and z angular velocity values to the screen below the title. Use f strings!
-# Don't forget to round the angular velocity values to three decimal places
 
-# send display group to screen
-display.show(splash)
+  title = f"z{mpu.gyro[2]} rad/s"
+  # the order of this command is (font, text, text color, and location)
+  text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=80)
+  splash.append(text_area)
+
+  # you will write more code here that prints the x, y, and z angular velocity values to the screen below the title. Use f strings!
+  # Don't forget to round the angular velocity values to three decimal places
+
+  # send display group to screen
+  display.show(splash)
