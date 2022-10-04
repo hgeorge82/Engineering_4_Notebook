01032680599b9a8e0723a1c65e59d1cb33df19bc
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-04 14:08:17 -0400

    OLED

--- raspberry-pi/Crash3.py
+++ raspberry-pi/Crash3.py
@@ -1,16 +1,24 @@
 #type: ignore
 from adafruit_display_text import label
 import adafruit_displayio_ssd1306 
+import adafruit_mpu6050
 import terminalio
 import displayio
 import board
 import time
 import busio
 
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(scl_pin, sda_pin)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+
 displayio.release_displays()
-display_bus = displayio.I2CDisplay(i2c, device_address=your-address-here, reset=board.your-pin-here)
+display_bus = displayio.I2CDisplay(i2c, device_address= 0x3d , reset=board.GP18)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
-mpu = adafruit_mpu6050.MPU6050(i2c, address=your-address-here)
+mpu = adafruit_mpu6050.MPU6050(i2c, address= 0x68 )
 
 # create the display group
 splash = displayio.Group()
@@ -20,6 +28,24 @@ title = "ANGULAR VELOCITY"
 # the order of this command is (font, text, text color, and location)
 text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
 splash.append(text_area)    
+
+
+title = "ANGULAR VELOCITY"
+# the order of this command is (font, text, text color, and location)
+text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=10)
+splash.append(text_area)
+
+
+title = "ANGULAR VELOCITY"
+# the order of this command is (font, text, text color, and location)
+text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=15)
+splash.append(text_area)
+
+
+title = "ANGULAR VELOCITY"
+# the order of this command is (font, text, text color, and location)
+text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=20)
+splash.append(text_area)
 
 # you will write more code here that prints the x, y, and z angular velocity values to the screen below the title. Use f strings!
 # Don't forget to round the angular velocity values to three decimal places
