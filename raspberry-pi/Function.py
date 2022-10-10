2528a9c2e96e8617932667fd3f919c662d5485b9
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-10 14:09:37 -0400

    Landing part 1

---
+++ raspberry-pi/Function.py
@@ -0,0 +1,58 @@
+#type: ignore
+from ast import Continue
+
+
+def print_sensor_values(axis):
+	if axis == "x":
+        print(f"X Acceleration: {mpu.acceleration[0]} m/s2")
+        print(f"X Angular Velocity: {mpu.gyro[0]} rad/s")
+        return mpu.acceleration[0], mpu.gyro[0]
+elif axis == "y":
+        print(f"X Acceleration: {mpu.acceleration[1]} m/s2")
+        print(f"X Angular Velocity: {mpu.gyro[1]} rad/s")
+        return mpu.acceleration[0], mpu.gyro[0]
+elif axis == "z":
+        print(f"X Acceleration: {mpu.acceleration[2]} m/s2")
+        print(f"X Angular Velocity: {mpu.gyro[2]} rad/s")
+        return mpu.acceleration[0], mpu.gyro[0]
+
+def triangle_area(x1y1, x2y2, x3y3):
+    try:
+    
+x1y1 = 
+x2y2 = 
+x3y3 =
+
+x1 = ("1")
+y1 = ("4.5")
+x2 = ("-2")
+y2 = ("9")
+x3 =("4")
+y3 = ("8")
+
+area =
+
+return area 
+
+
+except:
+    print()
+    
+    area = 0
+    return area 
+
+while True:
+    
+    
+val1 = (1,4.5)
+val2 = (-2,9)
+val3 = (4,8)
+
+area = triangle_area(val1, val2, val3)
+
+if area  == 0:
+ Continue
+ 
+ 
+else:
+    print()
