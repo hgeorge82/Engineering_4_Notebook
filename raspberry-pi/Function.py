00a7d882f2d602cc495226a715acbc9ae5397399
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-11 14:14:12 -0400

    launchpad

--- raspberry-pi/Function.py
+++ raspberry-pi/Function.py
@@ -1,58 +1,64 @@
-#type: ignore
-from ast import Continue
+# #type: ignore
+# from ast import Continue
 
 
-def print_sensor_values(axis):
-	if axis == "x":
-        print(f"X Acceleration: {mpu.acceleration[0]} m/s2")
-        print(f"X Angular Velocity: {mpu.gyro[0]} rad/s")
-        return mpu.acceleration[0], mpu.gyro[0]
-elif axis == "y":
-        print(f"X Acceleration: {mpu.acceleration[1]} m/s2")
-        print(f"X Angular Velocity: {mpu.gyro[1]} rad/s")
-        return mpu.acceleration[0], mpu.gyro[0]
-elif axis == "z":
-        print(f"X Acceleration: {mpu.acceleration[2]} m/s2")
-        print(f"X Angular Velocity: {mpu.gyro[2]} rad/s")
-        return mpu.acceleration[0], mpu.gyro[0]
+# def print_sensor_values(axis):
+# 	if axis == "x":
+#         print(f"X Acceleration: {mpu.acceleration[0]} m/s2")
+#         print(f"X Angular Velocity: {mpu.gyro[0]} rad/s")
+#         return mpu.acceleration[0], mpu.gyro[0]
+# elif axis == "y":
+#         print(f"X Acceleration: {mpu.acceleration[1]} m/s2")
+#         print(f"X Angular Velocity: {mpu.gyro[1]} rad/s")
+#         return mpu.acceleration[0], mpu.gyro[0]
+# elif axis == "z":
+#         print(f"X Acceleration: {mpu.acceleration[2]} m/s2")
+#         print(f"X Angular Velocity: {mpu.gyro[2]} rad/s")
+#         return mpu.acceleration[0], mpu.gyro[0]
 
-def triangle_area(x1y1, x2y2, x3y3):
-    try:
+# def triangle_area(x1y1, x2y2, x3y3):
+#     try:
     
-x1y1 = 
-x2y2 = 
-x3y3 =
+# x1y1 = 
+# x2y2 = 
+# x3y3 =
 
-x1 = ("1")
-y1 = ("4.5")
-x2 = ("-2")
-y2 = ("9")
-x3 =("4")
-y3 = ("8")
+# x1 = ("1")
+# y1 = ("4.5")
+# x2 = ("-2")
+# y2 = ("9")
+# x3 =("4")
+# y3 = ("8")
 
-area =
+# area =
 
-return area 
+# return area 
 
 
-except:
-    print()
+# except:
+#     print()
     
-    area = 0
-    return area 
+#     area = 0
+#     return area 
 
-while True:
+# while True:
     
     
-val1 = (1,4.5)
-val2 = (-2,9)
-val3 = (4,8)
+val1 = input("put coords in")
 
-area = triangle_area(val1, val2, val3)
+print (val1)
 
-if area  == 0:
- Continue
+txt = "welcome to the jungle"
+
+x = txt.split(val1)
+
+print(x)
+
+# area = triangle_area(val1, val2, val3)
+
+# if area  == 0:
+#  Continue
  
  
-else:
-    print()
+# else:
+#     print()
