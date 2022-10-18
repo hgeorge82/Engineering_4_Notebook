6f1e9d7621fe42a3d9bf4b58e05cd2d9e182fd88
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-18 14:11:51 -0400

    dfg

--- raspberry-pi/Function.py
+++ raspberry-pi/Function.py
@@ -1,45 +1,45 @@
 #type: ignore
-def triangle_area(x1y1, x2y2, x3y3)     
+def triangle_area(val1, val2,val3):   
 
-try:
-    val1 = input("Put coordinates in: ") 
-    val2 = input("Put coordinates in: ")
-    val3 = input("Put coordinates in: ")
+    try:
+        val1 = val1.split(",")
+        val2 = val2.split(",")
+        val3 = val3.split(",")
 
-    val1 = val1.split(",")
-    val2 = val2.split(",")
-    val3 = val3.split(",")
+        x1 = float(val1[0])
+        y1 = float(val1[1])
+        x2 = float(val2[0])
+        y2 = float(val2[1])
+        x3 = float(val3[0])
+        y3 = float(val3[1])
 
-    x1 = float(val1[0])
-    y1 = float(val1[1])
-    x2 = float(val2[0])
-    y2 = float(val2[1])
-    x3 = float(val3[0])
-    y3 = float(val3[1])
+        print(val1)
+        print(val2)
+        print(val3)
+        
+        area = (abs(x1*(y2 - y3)+x2*(y3 - y1)+x3*(y1 - y2)))/2
+        print(x1,y1,x2,y2,x3,y3)
+        
+        
+        return area
 
-    print(val1)
-    print(val2)
-    print(val3)
-    
-    area = (abs(x1*(y2 - y3)+x2*(y3 - y1)+x3*(y1 - y2)))/2
-    print(x1,y1,x2,y2,x3,y3)
-    
-    
-    return area
-
-except:
-    
-      print("An exception occurred")
+    except:
+        
+        print("An exception occurred")
+        area = 0 
+        return area
       
 
 while  True:
-    val1 = 
-    val2 =
-    val3 = 
+    val1 = input("Put coordinates in: ") 
+    val2 = input("Put coordinates in: ")
+    val3 = input("Put coordinates in: ")
     
-    area = area_triangle(val1, val2, val3)
+    area = triangle_area(val1, val2, val3)
     
-    if area == 0
-    
-    else: 
-        print
+    if area == 0:
+        continue
+    else:
+        print("The area of the triangle with vertices: ")
+        print(area)
+        
