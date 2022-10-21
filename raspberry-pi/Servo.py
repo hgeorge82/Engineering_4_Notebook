80bf5497a375b6ea9f2721375af99dbaa12d90bc
Hope <71407017+hgeorge82@users.noreply.github.com>
2022-10-21 10:12:27 -0400

    Update servo.py

--- raspberry-pi/Servo.py
+++ raspberry-pi/Servo.py
@@ -1,32 +1,32 @@
 # type: ignore
-import board
+import board 
 import digitalio
 import time
 import pwmio
-from adafruit_motor import servo
+from adafruit_motor import servo #imports code from adafruit_motor library. 
 
 
-ledgreen = digitalio.DigitalInOut(board.GP18)
+ledgreen = digitalio.DigitalInOut(board.GP18) #defining of the led lights
 ledgreen.direction = digitalio.Direction.OUTPUT
 ledred = digitalio.DigitalInOut(board.GP13)
 ledred.direction = digitalio.Direction.OUTPUT
 
-button = digitalio.DigitalInOut(board.GP16)
+button = digitalio.DigitalInOut(board.GP16) #defining of the button
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
 
-pwm_servo = pwmio.PWMOut(board.GP17, duty_cycle=2 ** 15, frequency=50)
-servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
+pwm_servo = pwmio.PWMOut(board.GP17, duty_cycle=2 ** 15, frequency=50) #powersource for the servo
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500) #tells what direction for the servo to go
 
 
-servo1.angle = 0
+servo1.angle = 0 # servo starts out as zero before loop is called
 
 
 while True: 
-  if button.value == True:
-    for x in range (10, -1, -1):
-      if x != 0:
+  if button.value == True: #when button is pressed 
+    for x in range (10, -1, -1): #calls values for countdown(index)
+      if x != 0: 
         ledred.value = True
         print(x)
         time.sleep(0.5)
@@ -34,7 +34,7 @@ while True:
         time.sleep(0.5)
       
       
-      else: 
+      else: # x doesn't equal zero so the else statemnet runs
         print("Liftoff!")
         ledgreen.value = False 
         print(x)
