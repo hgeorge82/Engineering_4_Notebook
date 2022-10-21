4015afd64b64b1fbc31d7b503fe241ed006185e1
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-21 14:09:45 -0400

    ghghj

--- raspberry-pi/Button.py
+++ raspberry-pi/Button.py
@@ -1,29 +1,29 @@
 # type: ignore
-import board
-import digitalio
-import time
-ledgreen = digitalio.DigitalInOut(board.GP18)
+import board ##
+import digitalio ##
+import time ## 
+ledgreen = digitalio.DigitalInOut(board.GP18) ## 
 ledgreen.direction = digitalio.Direction.OUTPUT
 ledred = digitalio.DigitalInOut(board.GP13)
 ledred.direction = digitalio.Direction.OUTPUT
 
-button = digitalio.DigitalInOut(board.GP16)
+button = digitalio.DigitalInOut(board.GP16) #
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
 
-while True: 
+while True: # 
   if button.value == True:
-    for x in range (10, -1, -1):
-      if x != 0:
+    for x in range (10, -1, -1): #
+      if x != 0: #
         ledred.value = True
         print(x)
         time.sleep(0.5)
         ledred.value = False
         time.sleep(0.5)
       
-      
-      else: 
+       
+      else: #
         print("Liftoff!")
         ledgreen.value = True
         print(x)
