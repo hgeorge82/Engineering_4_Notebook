a329f06d7d318706d64f76095d3ac0feacedcd60
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2022-10-21 14:08:59 -0400

    sdjhfsjk

--- raspberry-pi/Plotting.py
+++ raspberry-pi/Plotting.py
@@ -3,6 +3,15 @@ from adafruit_display_shapes.triangle import Triangle
 from adafruit_display_shapes.line import Line
 from adafruit_display_shapes.circle import Circle
 
+
+
+sda_pin = board.GP14
+scl_pin = board.GP15 
+i2c = busio.I2C(scl_pin, sda_pin)
+display_bus = displayio.I2CDisplay(i2c, device_address= 0x3d , reset=board.GP18)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+
+
 def triangle_area(val1, val2,val3):   
 
     try:
@@ -21,13 +30,13 @@ def triangle_area(val1, val2,val3):
         print(val2)
         print(val3)
         
-        hline = Line(x1,y1,x2,y2, color=0xFFFF00)
+        hline = Line(0,64,0,64, color=0xFFFF00)
         splash.append(hline)
         
-        circle = Circle(x, y, r, outline=0xFFFF00)
+        circle = Circle(64, 32, 0, outline=0xFFFF00)
         splash.append(circle)
         
-        triangle = Triangle(x1, y1, x2, y2, x3, y3, outline=0xFFFF00)
+        triangle = Triangle(val1, val2, val3, outline=0xFFFF00)
         splash.append(triangle)
 
         area = (abs(x1*(y2 - y3)+x2*(y3 - y1)+x3*(y1 - y2)))/2
