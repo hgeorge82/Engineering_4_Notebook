f3fa34c71da2637b8270150d4a52492a6d786b2b
hgeorge82 <hgeorge82@charlottesvilleschools.org>
2023-02-16 16:42:53 -0500

    dfd

--- raspberry-pi/SSA.py
+++ raspberry-pi/SSA.py
@@ -1,22 +1,29 @@
 # type: ignore
 import time 
+import os 
 import board
 import busio
 import adafruit_mpl3115a2
 import displayio
-
+import storage
 
 
 displayio.release_displays() 
 sda_pin = board.GP10
 scl_pin = board.GP11
 i2c = busio.I2C(scl_pin, sda_pin)  
-
+write_pin = digitalio.DigitalInOut(board.GP0)
+write_pin.direction = digitalio.Direction.INPUT
+write_pin.pull = digitalio.Pull.UP
 
 
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 #sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
 
+# If write pin is connected to ground on start-up, CircuitPython can write to
+CIRCUITPY filesystem.
+if not write_pin.value:
+ storage.remount("/", readonly=False)
 
 sensor.sealevel_pressure = 102250
 
@@ -29,3 +36,9 @@ while True:
     temperature = sensor.temperature
     print("Temperature: {0:0.3f} degrees Celsius".format(temperature))
     time.sleep(1.0)
+    break 
+ 
+Values=open(f"/data/{time.monotonic()}.csv","w")
+for i in range(len(list_z)):
+Values.write(f"{list_time[i]}{list_z[i]}\n")
+Values.close
